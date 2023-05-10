aa0a8e1244a200ba7c1e67392bf8a523973a1482
BestSTN <bestflashcs3@hotmail.com>
2023-05-10 16:11:25 +0700

    setup layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,36 @@
+import HomePage from "./pages/HomePage";
+
 import { useEffect, useState } from "react";
 import liff from "@line/liff";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./App.css";
 
 function App() {
-  const [message, setMessage] = useState("");
   const [error, setError] = useState("");
 
   useEffect(() => {
     liff
       .init({
-        liffId: import.meta.env.VITE_LIFF_ID
+        liffId: import.meta.env.VITE_LIFF_ID,
       })
-      .then(() => {
-        setMessage("LIFF init succeeded.");
-      })
+      .then(() => {})
       .catch((e) => {
-        setMessage("LIFF init failed.");
         setError(`${e}`);
       });
   });
 
   return (
     <div className="App">
-      <h1>create-liff-app</h1>
-      {message && <p>{message}</p>}
-      {error && (
+      {error ? (
         <p>
           <code>{error}</code>
         </p>
+      ) : (
+        <>
+          <HomePage />
+        </>
       )}
-      <a
-        href="https://developers.line.biz/ja/docs/liff/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        LIFF Documentation
-      </a>
     </div>
   );
 }
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import Header from "../features/header/Header";
+import Product from "../features/product/Product";
+import Cart from "../features/cart/Cart";
+
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+      <Product />
+      <Cart />
+    </>
+  );
+};
+
+export default HomePage;
